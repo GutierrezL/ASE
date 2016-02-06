de019eb3d0617a40135a2dcbb6e25ee2da498345
lindavik <linda.viksne@gmail.com>
2016-02-06 16:32:58 +0000

    MenuItem Constructor

--- Adv_soft_CW/src/MenuItem.java
+++ Adv_soft_CW/src/MenuItem.java
@@ -1,4 +1,18 @@
 
 public class MenuItem {
-
+	
+	private String itemName;
+	private double itemPrice;
+	private String category;
+	private boolean isVegetarian;
+	private static final String [] categories = new String[]{"Starter","Main", "Dessert", "Side", "Drink"};
+	
+	public MenuItem(String name, double price, String category, boolean is_vegetarian){
+		
+		itemName = name;
+		itemPrice = price;
+		this.category = category;
+		isVegetarian = is_vegetarian;	
+		
+	}
 }
