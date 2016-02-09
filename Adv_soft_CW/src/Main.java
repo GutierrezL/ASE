8f4db25cd8ca5a3d5c4b8243d03e705a7b5e5575
lindavik <linda.viksne@gmail.com>
2016-02-09 13:18:53 +0000

    Cleaned and commented 2.

--- Adv_soft_CW/src/Main.java
+++ Adv_soft_CW/src/Main.java
@@ -6,15 +6,6 @@ public class Main {
 		
 		MenuItemMap menuMap = new MenuItemMap();
 		
-		//MenuItem test1 = new MenuItem("Roasted vegetables", 3.5, "Side", true);
-		//MenuItem test2 = new MenuItem("Baked cheesecake", 4, "Dessert", true);
-		//menuMap.put(test1.getName(), test1);
-		//menuMap.put(test2.getName(), test2);
-		//menuMap.printMenu();
-		
-		//System.out.println(test1.printItemSummary());
-		//System.out.println(test2.printItemSummary());
-		
 		MenuScanner s = new MenuScanner();
 		menuMap = s.getMenuEntries();
 		System.out.println(menuMap.listByName());
