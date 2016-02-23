61645e3660c32a3ff2b5def6fddffa3fe0186087
lindavik <linda.viksne@gmail.com>
2016-02-23 19:58:56 +0000

    silly comment and output added

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 
 public class Main {
 
-	//This is a comment
-	//And this is another comment
-	//Third comment
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//blabla
 		
-		System.out.println("YAYY");
+		// dear lord, work already
+		System.out.println("Ha.");
 	}
 
 }
