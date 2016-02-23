c0feda611c1812d5bb97c3b4ad37f72d9a3267a5
lindavik <linda.viksne@gmail.com>
2016-02-23 16:30:43 +0000

    more comments
    m

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ public class Main {
 
 	//This is a comment
 	//And this is another comment
+	//Third comment
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//blabla
+		
+		System.out.println("YAYY");
 	}
 
 }
