c041538dfd1e29d3bec584134e86e2f6c25a2761
lindavik <linda.viksne@gmail.com>
2016-02-23 19:33:20 +0000

    Deleted irrelevant files

--- src/NoMatchingItem.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Exception Class for No Matching Item
- */
-public class NoMatchingItem extends Exception {
-	public NoMatchingItem (String NoItem){
-		super("No Item Found = " + NoItem);
-	}
-}
--- src/NoMatchingOrderID.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Exception Class for No Matching Order ID
- */
-public class NoMatchingOrderID extends Exception {
-	public NoMatchingOrderID(String NoId){
-		super("No ID Found = " + NoId);
-	}
-}
